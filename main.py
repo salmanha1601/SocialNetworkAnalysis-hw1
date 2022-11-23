75b182157951f76a5016593184671aacbf6411fe
salman havaqa <36137313+salmanha1601@users.noreply.github.com>
2022-11-23 15:21:29 +0200

    start project

---
+++ main.py
@@ -0,0 +1,16 @@
+def load_graph(path):
+    pass
+
+
+def calculate_page_rank(β, δ, maxIterations):
+    pass
+
+def get_PageRank(node_name):
+    pass
+
+def get_top_PageRank (n):
+    pass
+
+def get_all_PageRank():
+    pass
+
